8f98e9ef6964c2bc5ba44a93421509e1422c706d
gskidmor <gskidmor@ucsc.edu>
2020-08-28 21:02:40 -0700

    Completed 3rd test for SelSort (unequal)

--- SortingClassLibrary/src/UnitTesting.cpp
+++ SortingClassLibrary/src/UnitTesting.cpp
@@ -15,32 +15,50 @@ int Test_SelectionSort(void) {
 	// Test 1: Simple test 0-9
 	std::vector<int> sortArray = { 1, 0, 2, 3, 7, 5, 4, 6, 9, 8 }, ans = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	
-	// Now call Selection Sort on sortArray
+	// Now call Selection Sort on sortArray, recording the time the process takes
 	auto start = Clock::now();
 	SelectionSort(sortArray);
 	auto finish = Clock::now();
 
-	// Now verify equality
+	// Now verify equality and print out the results
 	if (Verify(sortArray, ans)) {
 		std::cout << "  Test 1 (Simple Test): PASSED  (" << std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count() << "us)" << std::endl;
 	}
 	else {
 		result = 0;
-		std::cout << "  Test 1 (Simple Test): FAILED" << std::endl;
+		std::cout << "  Test 1 (Simple Test): FAILED  (" << std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count() << "us)" << std::endl;
 	}
 
+
+	// Test 2: Char test
 	sortArray = { 'A', 'M', 'D', 'L', 'C', 'F', 'Z', 'I', 'H' }, ans = { 'A', 'C', 'D', 'F', 'H', 'I', 'L', 'M', 'Z' };
 	start = Clock::now();
 	SelectionSort(sortArray);
 	finish = Clock::now();
 
-	// Now verify equality
+	// Now verify equality and print out the results
 	if (Verify(sortArray, ans)) {
 		std::cout << "  Test 1 (Simple Test): PASSED  (" << std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count() << "us)" << std::endl;
 	}
 	else {
 		result = 0;
-		std::cout << "  Test 1 (Char Test): FAILED" << std::endl;
+		std::cout << "  Test 1 (Char Test): FAILED  (" << std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count() << "us)" << std::endl;
+	}
+
+
+	// Test 3: Unequal Vectors
+	sortArray = { 1, 3, 5, 6 }, ans = { 3, 5, 6, 9 };
+	start = Clock::now();
+	SelectionSort(sortArray);
+	finish = Clock::now();
+
+	// Now verify equality and print out the results
+	if (!Verify(sortArray, ans)) {
+		std::cout << "  Test 1 (Simple Test): PASSED  (" << std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count() << "us)" << std::endl;
+	}
+	else {
+		result = 0;
+		std::cout << "  Test 1 (Char Test): FAILED  (" << std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count() << "us)" << std::endl;
 	}
 
 	return result;
